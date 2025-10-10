f0e12e3ce796c316528c123aafef52c85806fd6b
Kate Wessels <katewessels58@gmail.com>
2025-10-10 12:36:59 -0700

    handle timestamp issue on ride edit

--- src/app/scheduled-rides/[rideId]/edit/page.jsx
+++ src/app/scheduled-rides/[rideId]/edit/page.jsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 import axios from "axios";
+import { DateTime } from "luxon";
+import ScheduledRideForm from "@/components/Rides/ScheduledRide/ScheduledRideForm";
 
 // export const metadata = {
 //   title: "Update ride Ride",
@@ -18,25 +20,18 @@ export default function EditScheduledRide() {
     // default is null, then populate on first load
     const [ride, setRide] = useState(null);
 
-    // default location options fro MVP
-    // TODO - also add coordinates for the default location list
-    const locationOptions = [
-        {city: 'San Francisco', state: 'CA'},
-        {city: 'Santa Cruz', state: 'CA'},
-        {city: 'Pacifica', state: 'CA'},
-        {city: 'Berkeley', state: 'CA'},
-        {city: 'Oakland', state: 'CA'},
-        {city: 'Fairfax', state: 'CA'}
-    ]
-
-    const typeOptions = ['mtb', 'gravel', 'road'];
+    const submitButtonText = 'Create';
 
 
     async function fetchScheduledRide () {
         try {
             const response = await axios.get(`/api/scheduled-rides/${rideId}`);
             console.log(response);
-            setRide(response.data);
+            setRide({...response.data,
+                    eventTime: DateTime.fromISO(response.data.eventTime, {zone: 'utc'})
+                                                            .setZone(response.data.timeZone)
+                                                            .toFormat("yyyy-MM-dd'T'HH:mm")
+            })
 
         } catch (err) {
             console.log('failed to get ride:', err);
@@ -50,7 +45,7 @@ export default function EditScheduledRide() {
 
 
 
-    const handleSubmit = async (e) => {
+    const handleUpdate = async (e) => {
         // TODO - add loading logic, button disabling logic
         // add handling to catch form submit without any required fields
         try {
@@ -83,145 +78,16 @@ export default function EditScheduledRide() {
         <div>
             <button onClick={() => router.push('/scheduled-rides')}>Back to Scheduled Rides</button>
             <h1>Update Scheduled Ride</h1>
-            <form onSubmit={handleSubmit} className="schedule-ride-form">
 
-                <label htmlFor="title">Title</label>
-                <input
-                    id="title"
-                    type="text"
-                    placeholder="title"
-                    value={ride.rideDetails.title}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    title: e.target.value}})}
-                    >
-                </input>
-
-                <label htmlFor="description">Description</label>
-                <textarea
-                    id="description"
-                    type="text"
-                    placeholder="description"
-                    value={ride.rideDetails.description}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    description: e.target.value}})}
-                    >
-                </textarea>
-
-                <label htmlFor="type">Type</label>
-                <select
-                    id="type"
-                    value={ride.rideDetails.type}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    type: e.target.value}})}
-                    >
-                        <option value="">Select ride type</option>
-                        {typeOptions.map((type) => (
-                            <option key={type}
-                                    value={type}>
-                                    {type}
-                                </option>
-                        ))}
-                </select>
-
-                <label htmlFor="link">Link</label>
-                <input
-                    id="link"
-                    type="text"
-                    placeholder="link"
-                    value={ride.rideDetails.link}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    link: e.target.value}})}
-                    >
-                </input>
-
-                <label htmlFor="notes">Notes</label>
-                <textarea
-                    id="notes"
-                    type="text"
-                    placeholder="notes"
-                    value={ride.rideDetails.notes}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    notes: e.target.value}})}
-                    >
-                </textarea>
-
-                <label htmlFor="location">Location</label>
-                <select
-                    id="location"
-                    value = {
-                        (ride.rideDetails.location.city && ride.rideDetails.location.state)
-                        ? `${ride.rideDetails.location.city}, ${ride.rideDetails.location.state}`
-                        : ""
-                    }
-                    onChange={(e) => {
-                        const [city, state] = e.target.value.split(',').map((str) => str.trim());
-                        setRide({...ride,
-                                rideDetails: {
-                                    ...ride.rideDetails,
-                                    location: {
-                                        ...ride.rideDetails.location,
-                                        city,
-                                        state
-                                    }
-                                }
-                                    })
-                            }}
-                    >
-                        <option value="">Select a city</option>
-                        {locationOptions.map((location) => (
-                            <option key={`${location.city}-${location.state}`}
-                                    value={`${location.city}, ${location.state}`}>
-                                    {location.city}, {location.state}
-                                </option>
-                        ))}
-                </select>
-
-                <label htmlFor="isPublic">Visibility</label>
-                <select
-                    id="isPublic"
-                    value={ride.isPublic}
-                    onChange={(e) => setRide({...ride, isPublic: e.target.value})}
-                    >
-                    <option value="">Select visibility</option>
-                    <option value="false">Private</option>
-                    <option value="true">Public</option>
-                </select>
-
-                <label htmlFor="eventTime">Event Time</label>
-                <input
-                    id="eventTime"
-                    type="datetime-local"
-                    value={ride.eventTime}
-                    onChange={(e) => setRide({...ride, eventTime: e.target.value})}>
-                </input>
-
-                <label htmlFor="timeZone">Time Zone</label>
-                <select
-                    id="timeZone"
-                    value={ride.timeZone}
-                    onChange={(e) => setRide({...ride, timeZone: e.target.value})}
+            <ScheduledRideForm
+                ride={ride}
+                setRide={setRide}
+                onSubmit={handleUpdate}
+                submitButtonText={submitButtonText}
                 >
-                    <option value="">Select a time zone</option>
-                    <option value="America/Los_Angeles">Pacific Time (PT)</option>
-                    <option value="America/Denver">Mountain Time (MT)</option>
-                    <option value="America/Chicago">Central Time (CT)</option>
-                    <option value="America/New_York">Eastern Time (ET)</option>
-                    <option value="UTC">UTC</option>
-                </select>
+            </ScheduledRideForm>
 
 
-                <button type="submit">Save Changes</button>
-            </form>
         </div>
     )
 }
--- src/app/scheduled-rides/new/page.jsx
+++ src/app/scheduled-rides/new/page.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import axios from "axios";
 import { DateTime } from "luxon";
+import ScheduledRideForm from "@/components/Rides/ScheduledRide/ScheduledRideForm";
 
 // export const metadata = {
 //   title: "Schedule New Ride",
@@ -11,8 +12,6 @@ import { DateTime } from "luxon";
 // };
 
 
-
-// create new ride
 
 export default function ScheduleNewRide() {
 
@@ -39,20 +38,9 @@ export default function ScheduleNewRide() {
 
     const [ride, setRide] = useState(defaultRide);
 
-    // default location options fro MVP
-    // TODO - also add coordinates for the default location list
-    const locationOptions = [
-        {city: 'San Francisco', state: 'CA'},
-        {city: 'Santa Cruz', state: 'CA'},
-        {city: 'Pacifica', state: 'CA'},
-        {city: 'Berkeley', state: 'CA'},
-        {city: 'Oakland', state: 'CA'},
-        {city: 'Fairfax', state: 'CA'}
-    ]
+    const submitButtonText = 'Create';
 
-    const typeOptions = ['mtb', 'gravel', 'road'];
 
-    // handle case when we have search query param of savedRideId to pre-populate form
     useEffect(() => {
         if (savedRideId) {
             const fetchRide = async () => {
@@ -81,9 +69,8 @@ export default function ScheduleNewRide() {
         }
     }, [savedRideId]);
 
-    // TODO add onchange handler that's general and re-usable for form jsx below
 
-    const onSubmit = async (e) => {
+    const handleCreate = async (e) => {
         // TODO - add loading logic, button disabling logic
         // add handling to catch form submit without any required fields
         try {
@@ -116,143 +103,15 @@ export default function ScheduleNewRide() {
         <div>
             <button onClick={() => router.push('/scheduled-rides')}>Back to Scheduled Rides</button>
             <h1>Schedule New Ride</h1>
-            <form className="schedule-ride-form">
-                <label htmlFor="title">Title</label>
-                <input
-                    id="title"
-                    type="text"
-                    placeholder="title"
-                    value={ride.rideDetails.title}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    title: e.target.value}})}
-                    >
-                </input>
 
-                <label htmlFor="description">Description</label>
-                <textarea
-                    id="description"
-                    type="text"
-                    placeholder="description"
-                    value={ride.rideDetails.description}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    description: e.target.value}})}
-                    >
-                </textarea>
-
-                <label htmlFor="type">Type</label>
-                <select
-                    id="type"
-                    value={ride.rideDetails.type}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    type: e.target.value}})}
-                    >
-                        <option value="">Select ride type</option>
-                        {typeOptions.map((type) => (
-                            <option key={type}
-                                    value={type}>
-                                    {type}
-                                </option>
-                        ))}
-                </select>
-
-                <label htmlFor="link">Link</label>
-                <input
-                    id="link"
-                    type="text"
-                    placeholder="link"
-                    value={ride.rideDetails.link}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    link: e.target.value}})}
-                    >
-                </input>
-
-                <label htmlFor="notes">Notes</label>
-                <textarea
-                    id="notes"
-                    type="text"
-                    placeholder="notes"
-                    value={ride.rideDetails.notes}
-                    onChange={(e) => setRide({...ride,
-                                            rideDetails: {
-                                                ...ride.rideDetails,
-                                                    notes: e.target.value}})}
-                    >
-                </textarea>
-
-                <label htmlFor="location">Location</label>
-                <select
-                    id="location"
-                    value = {
-                        (ride.rideDetails.location.city && ride.rideDetails.location.state)
-                        ? `${ride.rideDetails.location.city}, ${ride.rideDetails.location.state}`
-                        : ""
-                    }
-                    onChange={(e) => {
-                        const [city, state] = e.target.value.split(',').map((str) => str.trim());
-                        setRide({...ride,
-                                rideDetails: {
-                                    ...ride.rideDetails,
-                                    location: {
-                                        ...ride.rideDetails.location,
-                                        city,
-                                        state
-                                    }
-                                }
-                                    })
-                            }}
-                    >
-                        <option value="">Select a city</option>
-                        {locationOptions.map((location) => (
-                            <option key={`${location.city}-${location.state}`}
-                                    value={`${location.city}, ${location.state}`}>
-                                    {location.city}, {location.state}
-                                </option>
-                        ))}
-                </select>
-
-                <label htmlFor="isPublic">Visibility</label>
-                <select
-                    id="isPublic"
-                    value={ride.isPublic}
-                    onChange={(e) => setRide({...ride, isPublic: e.target.value})}
-                    >
-                    <option value="">Select visibility</option>
-                    <option value="false">Private</option>
-                    <option value="true">Public</option>
-                </select>
-
-                <label htmlFor="eventTime">Event Time</label>
-                <input
-                    id="eventTime"
-                    type="datetime-local"
-                    value={ride.eventTime}
-                    onChange={(e) => setRide({...ride, eventTime: e.target.value})}>
-                </input>
-
-                <label htmlFor="timeZone">Time Zone</label>
-                <select
-                    id="timeZone"
-                    value={ride.timeZone}
-                    onChange={(e) => setRide({...ride, timeZone: e.target.value})}
+            <ScheduledRideForm
+                ride={ride}
+                setRide={setRide}
+                onSubmit={handleCreate}
+                submitButtonText={submitButtonText}
                 >
-                    <option value="">Select a time zone</option>
-                    <option value="America/Los_Angeles">Pacific Time (PT)</option>
-                    <option value="America/Denver">Mountain Time (MT)</option>
-                    <option value="America/Chicago">Central Time (CT)</option>
-                    <option value="America/New_York">Eastern Time (ET)</option>
-                    <option value="UTC">UTC</option>
-                </select>
+            </ScheduledRideForm>
 
-                <button onClick={onSubmit}>Schedule</button>
-            </form>
         </div>
     )
 }

31226342b98879dcde195f3d97d1cf95b975c2ea
Kate Wessels <katewessels58@gmail.com>
2025-10-15 10:44:17 -0700

    adding favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
     default: "BikeBuddy",
     template: "%s | BikeBuddy"
   },
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚲</text></svg>'
+  },
   description: "Find and schedule rides in your area"
 };
 

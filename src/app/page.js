aabd428680126ba01a7f3d253e7b737be86c6297
Kate Wessels <katewessels58@gmail.com>
2025-10-14 18:58:40 -0700

    adding a little more to home page

--- src/app/page.js
+++ src/app/page.js
@@ -11,24 +11,53 @@ export default function HomePage() {
 
       <h1>BikeBuddy</h1>
       <div className='card'>
-        <p>
+        <p className='home-description'>
         An app for riders (MTB, Gravel, Road) to plan and join rides in their area.
         </p>
 
         {!authUser &&
-          <>
-            <p>
-              Join BikeBuddy today.          <Link href="/signup">
-                <button className='btn-primary'>Sign Up</button>
+          <div className='home-actions'>
+            <div className='home-action-card'>
+              <span>Join BikeBuddy today</span>
+              <Link href="/signup">
+                  <button className='btn-primary'>Sign Up</button>
               </Link>
-            </p>
-              <p>
-                Already have an account?          <Link href="/login">
-                  <button className='btn-primary'>Log In</button>
-                </Link>
+            </div>
 
-            </p>
-          </>
+            <div className='home-action-card'>
+              <span> Already have an account? </span>
+              <Link href="/login">
+                <button className='btn-primary'>Log In</button>
+              </Link>
+            </div>
+          </div>
+        }
+
+
+        {authUser &&
+          <div className='home-actions'>
+
+            <div className='home-action-card'>
+              <span> Browse and add to your personal saved rides </span>
+              <Link href="/saved-rides">
+                <button className='btn-primary'>Saved Rides</button>
+              </Link>
+            </div>
+
+            <div className='home-action-card'>
+              <span> Schedule public or private rides </span>
+              <Link href="/scheduled-rides">
+                    <button className='btn-primary'>Scheduled Rides</button>
+              </Link>
+            </div>
+
+            <div className='home-action-card'>
+              <span> Check out the public ride feed and join rides in your area </span>
+              <Link href="/ride-feed">
+                  <button className='btn-primary'>Ride Feed</button>
+              </Link>
+            </div>
+          </div>
         }
       </div>
 
